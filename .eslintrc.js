719d7f7f86af8259b95ef840ce859f77262e3f68
कारतोफ्फेलस्क्रिप्ट™ <aditya@netroy.in>
2024-07-11 17:44:31 +0200

    Upgrade dependencies. Sync with n8n-nodes-starter

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+/**
+ * @type {import('@types/eslint').ESLint.ConfigData}
+ */
+module.exports = {
+	root: true,
+
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+	},
+
+	parser: '@typescript-eslint/parser',
+
+	parserOptions: {
+		project: ['./tsconfig.json'],
+		sourceType: 'module',
+		extraFileExtensions: ['.json'],
+	},
+
+	ignorePatterns: ['.eslintrc.js', '**/*.js', '**/node_modules/**', '**/dist/**'],
+
+	overrides: [
+		{
+			files: ['package.json', 'credentials/**/*.ts', 'nodes/**/*.ts']
+		},
+	],
+};
